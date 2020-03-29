01033855cb8f22fb836cb0d646c92069e0ea6aec
yatsiuk-oa <yatsiuk.oa@gbksoft.com>
2020-03-29 23:35:07 +0300

    config sync

--- test/specs/FacebookSeachAndRepost.js
+++ test/specs/FacebookSeachAndRepost.js
@@ -1,42 +1,44 @@
 const assert = require('assert')
 
 describe('Testing Facebook searching people', () => {
-    it('I open Facebook autorization form', async () => {
-        await browser.url('/')
-        const title = await browser.getTitle()
+    it('I open Facebook autorization form', () => {
+        browser.url('/')
+        const title = browser.getTitle()
+        console.log(title)
         assert.strictEqual(title, 'Facebook - Log In or Sign Up')
     })
-    it('I write login and password in the form', async () => {
-        const login = await $('#email');
+    it('I write login and password in the form', () => {
+        const login = $('#email');
         login.setValue('pavel_chorniy@i.ua');
-        const password = await $('#pass');
+        const password = $('#pass');
         password.setValue('TORNADO_123');
     })
-    it('I press "Login" button', async () => {
-        const myButton = await $('input[type="submit"]');
+    it('I press "Login" button', () => {
+        const myButton = $('input[type="submit"]');
+        myButton.click();
+        browser.pause(1000)
+    })
+    it('I wait for visible modal window', () => {
+        const elem = $('div[class="__xn"]')
+        elem.waitForDisplayed({ timeout: 2000 });
+    })
+    it('I close modal window', () => {
+        const myButton = $('button[class="layer_close_elem _36gl _50zy _50-0 _50z_ _5upp _42ft"]');
         myButton.click();
     })
-    it('I wait for visible modal window', async () => {
-        const elem = await $('div[class="__xn"]')
-        await elem.waitForDisplayed({ timeout: 2000 });
-    })
-    it('I close modal window', async () => {
-        const myButton = await $('button[class="layer_close_elem _36gl _50zy _50-0 _50z_ _5upp _42ft"]');
-        myButton.click();
-    })
-    it('I write the name of group', async () => {
-        const value = await $('input[name="q"]');
+    it('I write the name of group', () => {
+        const value = $('input[name="q"]');
         value.setValue('автопром');
     })
-    it ('I press search button', async () => {
-    const myButton = await $('button[aria-label="Поиск"]');
+    it('I press search button', () => {
+        const myButton = $('button[aria-label="Поиск"]');
         myButton.click();
-        await browser.pause(3000);
+        browser.pause(3000);
     })
-    it('Open searchs results', async () => {
-        const myButton = await $('a[href="https://www.facebook.com/avto.prom.1"]');
+    it('Open searchs results', () => {
+        const myButton = $('a[href="https://www.facebook.com/avto.prom.1"]');
         myButton.click();
-        await browser.pause(2000);
+        browser.pause(2000);
     })
     it('Find the post for repost and repost it', async () => {
         const shareButton = await $$('._2nj7._18vj');
@@ -44,19 +46,18 @@ describe('Testing Facebook searching people', () => {
         await browser.pause(3000);
         shareButton[1].click();
         await browser.pause(3000);
-        // await shareButtonn.click();
         const shareMyPage = await $('._3gc- > a');
         shareMyPage.click();
+        await browser.pause(2000);
         const publicate = await $('._2g61');
         publicate.click();
         await browser.pause(1000);
     })
-    it('I wait for acepting publication and check it', async () => {
-        const textForAccepting = await $('._4-i2');
-        const popUpWindow = await textForAccepting.getText();
-        assert.equal(popUpWindow,'Опубликовано на Странице pasha_chorniy.');
-        const linkToPage = await $('._4-i2 a');
+    it('I wait for acepting publication and check it', () => {
+        const textForAccepting = $('._4-i2');
+        const popUpWindow = textForAccepting.getText();
+        assert.equal(popUpWindow, 'Опубликовано на Странице pasha_chorniy.');
+        const linkToPage = $('._4-i2 a');
         linkToPage.click();
     })
- 
-    })
+})
